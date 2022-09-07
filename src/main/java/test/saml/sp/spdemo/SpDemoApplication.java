aee440a7c04aae48eacf31f2e518744964f89f11
CHENAL Benoit <benoit.chenal@ext.ec.europa.eu>
2022-09-07 15:54:37 +0200

    first commit

---
+++ src/main/java/test/saml/sp/spdemo/SpDemoApplication.java
@@ -0,0 +1,13 @@
+package test.saml.sp.spdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/saml/sp/spdemo/SpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package test.saml.sp.spdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
